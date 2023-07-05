ce8b88a7ecc51b7a017bbe01167959d1f93c3364
Alex Bihas <alex.bigas.2004@gmail.com>
2023-07-05 14:27:37 +0300

    Initial project UI setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder="number a" />
+      <select>
+        <option>+</option>
+        <option>-</option>
+        <option>x</option>
+        <option>:</option>
+      </select>
+      <input placeholder="number b" />
+      <input placeholder="result" disabled />
+      <button>Calculate</button>
+      <p>Calculator used: 100 times</p>
     </div>
   );
 }
